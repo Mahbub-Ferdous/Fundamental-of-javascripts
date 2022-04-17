cd259ef541f8d9d225523672695739e25a449076
Mahbub-Ferdous <mahbubferdous14@gmail.com>
2022-04-18 00:45:14 +0600

    js-operatos.js file added

---
+++ intro-of-javascript/js-operators.js
@@ -0,0 +1,60 @@
+// In javascript there have 5 types of operator :
+/*
+1. Arithmatic Operators :- Arithmetic operators are used to perform arithmetic on numbers like addition, substraction, division, multiply etc
+
+1. +    Addition
+2. -    KKSubtraction
+3. *    Multiplication
+4. **   Exponentiation (ES2016)
+5. /    Division
+6. %    Modulus (Division Remainder)
+7. ++   Increment
+8. --   Decrement
+
+
+
+2. Assignment Operator :- Assignment operators assign values to JavaScript variables
+
+1. =    x = y      x = y
+2. +=   x += y     x = x + y
+3. -=   x -= y     x = x - y
+4. *=   x *= y     x = x * y
+5. /=   x /= y     x = x / y
+6. %=   x %= y     x = x % y
+7. **=  x **= y    x = x ** y
+
+
+3. Comparison Operators: Comparison operator mainly use in condition statements and getting boolen data type. basically its use to compare one value to another value
+
+1. ==       equal to
+2. ===      equal value and equal type
+3. !=       not equal
+4. !==      not equal value or not equal type
+5. >        greater than
+6. <        less than
+7. >=       greater than or equal to
+8. <=       less than or equal to
+9. ?        ternary operator
+
+
+4. Bitwise operator:- Bit operators work on 32 bits numbers.Any numeric operand in the operation is converted into a 32 bit number. The result is converted back to a JavaScript number.
+
+
+1. &        AND                     5 & 1      0101 & 0001      0001        1
+2. |        OR                      5 | 1      0101 | 0001      0101        5
+3. ~        NOT                     5          ~0101            1010        10
+4. ^        XOR                     5 ^ 1      0101 ^ 0001      0100        4
+5. <<       left shift              5 << 1     0101 << 1        1010        10
+6. >>       right shift             5 >> 1     0101 >> 1        0010         2
+7. >>>      unsigned right shift    5 >>> 1    0101 >>> 1       0010         2
+
+
+5. Logical operators :- Logical operators are mainly and, or, not 
+
+
+1. &&       logical and
+2. ||       logical or
+3. !        logical not
+
+
+*/
