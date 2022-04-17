3588faa33969e095a3b59741ddc1de761e7dfe68
Mahbub-Ferdous <mahbubferdous14@gmail.com>
2022-04-18 00:23:02 +0600

    javascript fundamental - variables and data types

---
+++ intro-of-javascript/variable.js
@@ -0,0 +1,36 @@
+// Varibles in javascript
+
+// variable = vary + able  => thats means changeable and contains  different data type
+
+// declare a varible without storing data only declared
+var carName;
+
+// declare a varible with data type :
+var carName = "volvo";
+
+// if we declare a varible than we should know about 5 things first
+
+// {varible-keyword(var, let, const)} + {variableName} + {(= means assignment)} + {data with data type} + {;}
+var x = 10;
+let carName = "scania";
+const is_there = true;
+
+/* 
+varible declaration rules :
+1. Names can contain letters, digits, underscores, and dollar signs.
+2. Names must begin with a letter
+3. Names can also begin with $ and _ (but we will not use it in this tutorial)
+4. Names are case sensitive (y and Y are different variables)
+5. Reserved words (like JavaScript keywords) cannot be used as names
+6. JavaScript identifiers are case-sensitive.
+7. Name should be one word
+*/
+
+// javascripts data types : there have three types of data type in js = 1. 'string' 2. integer(numaric) 3. boolean
+
+/* 
+fundamental data types are:
+1. 'string' :- string means rope that means one or more sentence called string and we aleays binding our string with single or double cautation like - 'my name is mahbub' and  "i am from dhaka"
+2. Numaric/integer :- all kind of number with decimal or non dedcimal all are numaric data and we dont binding that with any block quatation
+3. Boolean :- boolean means true or false which is mostly use in conditional programming where true = 1 and false = 0.
+*/
